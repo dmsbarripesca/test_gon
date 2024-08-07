818d70d9cc5327115654fbdf19b47edd439ab238
dmsbarripesca <dmsbarripesca@gmail.com>
2024-08-07 17:26:51 +0200

    Add files via upload

---
+++ suma_y_sigue.py
@@ -0,0 +1,17 @@
+import asyncio
+import os
+
+
+#ABRIMOS Y LEEMOS EL TXT
+with open('numero.txt', 'r') as file:
+    numero_leido = file.read()
+
+#SUMAMOS 1 AL NÚMERO LEIDO
+numero_leido = int(numero_leido)
+numero_a_escribir = numero_leido + 1
+numero_a_escribir = str(numero_a_escribir)
+
+#SOBRESCRIBIMOS EL TXT CON EL NÚMEOR QUE LEIMOS + 1
+with open('numero.txt', 'w') as file:
+    numero_leido = file.write(numero_a_escribir)
+
